6081c33c548e9c70ac39d2c1480448f68ebfd2b7
Nandhakumar <Nandhakumar@TBS-32>
2023-10-04 16:31:19 +0530

    update

--- Parse EMV/Program.cs
+++ Parse EMV/Program.cs
@@ -1,67 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 class TLV
 {
+    public string Name { get; set; }
     public string Id { get; set; }
     public string Length { get; set; }
-    public string Value { get; set; }
-    public string Name { get; set; }
     public string TagIdSecondHalf { get; set; }
+    public string Value { get; set; }
 }
 
 class Program
 {
     static void Main()
     {
-        string emvData = "9F0607A000000003201057114507788153899851D2411226100006740F5A0845077881538998515F3401019F2701809F26084657B7AAD0991B609F100706010A03A0080082023C009F360200078C159F02069F03069F1A0295055F2A029A039C019F37049F02060000000000009F03060000000000009F1A020414950580000480005F2A0204149A032105249C01019F3704E44839E49F33036040209B02680050105669736120456C656374726F6E2044499F1E083535353335323138";
+        string filePath = "C:\\zoo\\emv-tags.txt";
 
-        List<TLV> emvTags = new List<TLV>();
-
-        emvTags.Add(new TLV { Id = "9F06", Name = "Application Identifier (AID)" });
-        emvTags.Add(new TLV { Id = "5F34", Name = "Application Transaction Counter (ATC)" });
-        emvTags.Add(new TLV { Id = "9F27", Name = "Cryptogram Information Data" });
-        emvTags.Add(new TLV { Id = "9F26", Name = "Application Cryptogram" });
-        emvTags.Add(new TLV { Id = "9F10", Name = "Issuer Application Data" });
-        emvTags.Add(new TLV { Id = "9F36", Name = "Application Transaction Counter (ATC) (Extended)" });
-        emvTags.Add(new TLV { Id = "9F02", Name = "Amount, Authorized (Numeric)" });
-        emvTags.Add(new TLV { Id = "9F03", Name = "Amount, Other (Numeric)" });
-        emvTags.Add(new TLV { Id = "9F1A", Name = "Terminal Country Code" });
-        emvTags.Add(new TLV { Id = "5F2A", Name = "Transaction Currency Code" });
-        emvTags.Add(new TLV { Id = "9F37", Name = "Unpredictable Number" });
-        emvTags.Add(new TLV { Id = "9F33", Name = "Terminal Capabilities" });
-        emvTags.Add(new TLV { Id = "9F1E", Name = "Interface Device (IFD) Serial Number" });
-        emvTags.Add(new TLV { Id = "57", Name = "Track Data" });
-        emvTags.Add(new TLV { Id = "5A", Name = "Application PAN" });
-        emvTags.Add(new TLV { Id = "82", Name = "Application Interchange Profile" });
-        emvTags.Add(new TLV { Id = "8C", Name = "Card Risk Management Data" });
-        emvTags.Add(new TLV { Id = "95", Name = "Terminal Verification Results (TVR)" });
-        emvTags.Add(new TLV { Id = "9A", Name = "Transaction Date" });
-        emvTags.Add(new TLV { Id = "9C", Name = "Transaction Type" });
-        emvTags.Add(new TLV { Id = "9B", Name = "Transaction Status Information" });
-        emvTags.Add(new TLV { Id = "50", Name = "Application Label" });
-
-        (List<TLV> non9F5FTags, List<TLV> _9F5FTags) = ParseEMVData(emvData, emvTags);
-
-        // Print the parsed tags
-        Console.WriteLine("SPECS Tags:");
-        foreach (var tag in non9F5FTags)
+        try
         {
-            Console.WriteLine($"Tag ID: {tag.Id}");
-            Console.WriteLine($"Tag Name: {tag.Name}");
-            Console.WriteLine($"Tag Length: {tag.Length}");
-            Console.WriteLine($"Tag Value: {tag.Value}");
+            string emvData = File.ReadAllText(filePath);
+
+            List<TLV> emvTags = new List<TLV>
+            {
+                new TLV { Id = "9F06", Name = "Application Identifier (AID)" },
+                new TLV { Id = "5F34", Name = "Application Transaction Counter (ATC)" },
+                new TLV { Id = "9F27", Name = "Cryptogram Information Data" },
+                new TLV { Id = "9F26", Name = "Application Cryptogram" },
+                new TLV { Id = "9F10", Name = "Issuer Application Data" },
+                new TLV { Id = "9F36", Name = "Application Transaction Counter (ATC) (Extended)" },
+                new TLV { Id = "9F02", Name = "Amount, Authorized (Numeric)" },
+                new TLV { Id = "9F03", Name = "Amount, Other (Numeric)" },
+                new TLV { Id = "9F1A", Name = "Terminal Country Code" },
+                new TLV { Id = "5F2A", Name = "Transaction Currency Code" },
+                new TLV { Id = "9F37", Name = "Unpredictable Number" },
+                new TLV { Id = "9F33", Name = "Terminal Capabilities" },
+                new TLV { Id = "9F1E", Name = "Interface Device (IFD) Serial Number" },
+                new TLV { Id = "57", Name = "Track Data" },
+                new TLV { Id = "5A", Name = "Application PAN" },
+                new TLV { Id = "82", Name = "Application Interchange Profile" },
+                new TLV { Id = "8C", Name = "Card Risk Management Data" },
+                new TLV { Id = "95", Name = "Terminal Verification Results (TVR)" },
+                new TLV { Id = "9A", Name = "Transaction Date" },
+                new TLV { Id = "9C", Name = "Transaction Type" },
+                new TLV { Id = "9B", Name = "Transaction Status Information" },
+                new TLV { Id = "50", Name = "Application Label" }
+                // Add more TLV objects as needed
+            };
+
+            (List<TLV> non9F5FTags, List<TLV> _9F5FTags) = ParseEMVData(emvData, emvTags);
+
+            // Print the parsed tags
+            Console.WriteLine("Non-9F5F Tags:");
+            foreach (var tag in non9F5FTags)
+            {
+                Console.WriteLine($"Tag ID: {tag.Id}");
+                Console.WriteLine($"Tag Length: {tag.Length}");
+                Console.WriteLine($"Tag Name: {tag.Name}");
+                Console.WriteLine($"Tag Value: {tag.Value}");
+            }
+
+            Console.WriteLine("\n9F5F Tags:");
+            foreach (var tag in _9F5FTags)
+            {
+                Console.WriteLine($"Tag ID: {tag.Id}");
+                Console.WriteLine($"Tag Length: {tag.Length}");
+                Console.WriteLine($"Tag Name: {tag.Name}");
+                Console.WriteLine($"Tag Value: {tag.Value}");
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("File not found. Please provide a valid file path.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
         }
 
-        Console.WriteLine("\nEMV Tags:");
-        foreach (var tag in _9F5FTags)
-        {
-            Console.WriteLine($"Tag ID: {tag.Id}");
-            Console.WriteLine($"Tag Name: {tag.Name}");
-            Console.WriteLine($"Tag Length: {tag.Length}");
-            Console.WriteLine($"Tag Value: {tag.Value}");
-        }
-        Console.ReadKey();
+        Console.ReadLine();
     }
 
     static (List<TLV>, List<TLV>) ParseEMVData(string emvData, List<TLV> emvTags)
@@ -84,32 +101,24 @@ class Program
                 tagId = tagId + tagIdSecondHalf;
             }
 
-            string name = "";
-            foreach (var tag in emvTags)
-            {
-                if (tagId == tag.Id)
-                {
-                    name = tag.Name;
-                }
-            }
-
             string tagLengthStr = emvData.Substring(index, 2);
             index += 2;
-            string tagLength = tagLengthStr.PadLeft(2, '0'); // Ensure two-character representation
+            string tagLength = tagLengthStr.PadLeft(2, '0');
 
-            int tagLengthValue = Convert.ToInt32(tagLength, 16); // Store the actual integer value if needed
+            int tagLengthValue = Convert.ToInt32(tagLength, 16);
             string tagValue = emvData.Substring(index, tagLengthValue * 2);
 
             index += tagLengthValue * 2;
 
             TLV emvTag = new TLV
             {
-                Id = tagId,
+                Id = tagId, // Use only the tagId without tagIdSecondHalf
                 TagIdSecondHalf = tagIdSecondHalf,
                 Length = tagLength,
                 Value = tagValue,
-                Name = name
+                Name = GetTagName(emvTags, tagId) // Get the name from the emvTags list
             };
+
             if (tagId.StartsWith("9F") || tagId.StartsWith("5F"))
             {
                 _9F5FTags.Add(emvTag);
@@ -118,9 +127,15 @@ class Program
             {
                 non9F5FTags.Add(emvTag);
             }
-
         }
 
         return (non9F5FTags, _9F5FTags);
+    }
+
+    static string GetTagName(List<TLV> emvTags, string tagId)
+    {
+        // Find the tag with the specified tagId and return its Name property
+        var tag = emvTags.Find(t => t.Id == tagId);
+        return tag?.Name ?? "";
     }
 }
